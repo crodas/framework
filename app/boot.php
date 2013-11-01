891251f071d03690e8d4f964a45c2140e15da17c
César D. Rodas <crodas@php.net>
2013-11-01 03:24:10 -0300

    added more libraries

--- app/boot.php
+++ app/boot.php
@@ -2,8 +2,19 @@
 
 require __DIR__ . "/../vendor/autoload.php";
 
+use Whoops\Run;
+use Whoops\Handler\PrettyPageHandler;
+
 $service = new \ServiceProvider\Composer(
     __DIR__ . "/configs/app.yml",
     'Service',
     __DIR__ . "/../tmp/services.php"
 );
+
+if (Service::Get('devel')) {
+    $run     = new Run;
+    $handler = new PrettyPageHandler;
+    $run->pushHandler($handler);
+    $run->register();
+}
+
